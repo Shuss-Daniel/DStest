4a7c0fa7d760a85361aaccd9a1e90994e4385831
P7145PC\DS <dshuss1@gmail.com>
2017-01-27 09:53:12 -0500

    01272017 first Commit on dsTest

---
+++ dsTest/src/dstest/DsTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dstest;
+
+import byui.cit260.dsTest.model.*;
+
+/**
+ *
+ * @author DS
+ */
+public class DsTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Room room1 = new Room();
+        savePlayer player1 = new savePlayer();
+        player1.setName("BillyBob");
+        player1.setScore(7);
+        String playerInfo = player1.toString();
+        System.out.println(playerInfo);
+    }
+    
+}
