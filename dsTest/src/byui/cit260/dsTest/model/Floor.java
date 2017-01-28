84445e0f21930abcf4a793c0081158849d381d31
P7145PC\DS <dshuss1@gmail.com>
2017-01-27 20:17:02 -0500

    01272017 second commit after adding 3 more classes

---
+++ dsTest/src/byui/cit260/dsTest/model/Floor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.dsTest.model;
+
+/**
+ *
+ * @author DS
+ */
+public class Floor {
+// class instance variables
+    private boolean roomNumber;
+ 
+// default constructor
+    public Floor() {
+    }
+    
+// Getters and Setters
+    public boolean isRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(boolean roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    
+}
---
+++ dsTest/src/byui/cit260/dsTest/model/GateKeeper.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.dsTest.model;
+
+/**
+ *
+ * @author DS
+ */
+public class GateKeeper {
+    
+// class instance variables
+    private boolean hasSolved;
+    
+// default constructor
+
+    public GateKeeper() {
+    }
+
+// Getters and Setters
+    public boolean isHasSolved() {
+        return hasSolved;
+    }
+
+    public void setHasSolved(boolean hasSolved) {
+        this.hasSolved = hasSolved;
+    }
+    
+}
